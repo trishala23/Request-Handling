a275be63e7c046373056aa87e49bf37a6be2fc6b
Trishala Kumari <53313889+trishala23@users.noreply.github.com>
2026-08-21 20:18:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,7 @@
-import { defineConfig } from 'vite';
-export default defineConfig({ base: './', test: { environment: 'node' }, esbuild: { jsx: 'automatic' } });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/java-backend-study-guide/',
+})
