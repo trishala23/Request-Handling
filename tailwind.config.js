bb11fd6715c9c270ca0ad2124cb70670fcdb5986
Kumari Trishala <trishala.kumari@mercedes-benz.com>
2026-08-22 16:34:31 +0530

    b

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-export default { content: ['./index.html','./src/**/*.{ts,tsx}'], theme: { extend: {} }, plugins: [] };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+//comment
